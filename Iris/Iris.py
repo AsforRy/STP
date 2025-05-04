4707cdd7e4406fd56f45c01ad71de472aebd7a03
Asfor <cbb111213@nptu.edu.tw>
2025-05-04 21:27:19 +0800

    To Line55 備註

--- Iris/Iris.py
+++ Iris/Iris.py
@@ -1,29 +1,60 @@
+#Import Required Libraries
+
+#匯入 pandas 套件，常用於讀取與處理表格型資料(ex : csv)
 import pandas as pd
+#匯入 NumPy，提供支援陣列、數值運算、統計等功能
 import numpy as np
+#匯入 os 模組，讓你能操作系統路徑、檔案、目錄等
 import os
+#匯入 matplotlib 的 pyplot 模組，通常用來畫圖或資料視覺化
 import matplotlib.pyplot as plt
+#匯入 seaborn，它是建立在 matplotlib 之上的統計視覺化套件，風格較美觀
 import seaborn as sns
+#關閉所有警告訊息
 import warnings
 warnings.filterwarnings('ignore')
 
+#Load the dataset
+
+#使用 pandas 讀取名為 Iris.csv 的 CSV 檔案，並存入 df（dataframe）變數中
 df = pd.read_csv('Iris.csv')
+#顯示資料表（dataframe）前 5 筆資料，方便快速瀏覽欄位與數據格式
 df.head()
 
-# Drop the 'Id' column as it is not required for analysis
+#移除資料表中不需要的 Id 欄位，因為這個欄位只是編號，不影響分析
 df = df.drop(columns=["Id"])
+#再次顯示前 5 筆資料（這時 Id 欄位已經移除了）
 df.head()
 
-#Display the first 10 rows of the dataframe
+#顯示資料表中 前 10 筆資料，可以看到更多花卉樣本的詳細數據
 df.head(10)
 
-# Display basic statistics about the data
+"""
+使用 pandas 的 describe() 方法，顯示數值型欄位的基本統計資料，包括：
+count：非空值的數量
+mean：平均值
+std：標準差（資料離散程度）
+min：最小值
+25%、50%（中位數）、75%：分位數（描述資料分布）
+max：最大值
+通常用來快速了解資料的範圍與分布
+"""
 df.describe()
 
-# Display information about the datatype of each column and null values
+""""
+使用 info() 方法來顯示資料表的整體結構資訊，包括：
+每個欄位的名稱與順序
+欄位的資料型態（例如 float64, int64, object 等）
+非空值的筆數（可用來判斷是否有缺漏資料）
+記憶體使用量
+非常適合用來檢查是否有 缺失值 或 類別資料
+"""
 df.info()
 
-# Display the number of samples for each class
+#顯示 Species 欄位中每個類別（花卉品種）出現的次數
 df['Species'].value_counts()
+
+#Preprocessing the Dataset
 
 #Label encoding to convert class labels into numeric form
 from sklearn.preprocessing import LabelEncoder
@@ -35,6 +66,8 @@ df
 
 # check for null values
 df.isnull().sum()
+
+#Exploratory Data Analysis (EDA)
 
 # Plot histograms of each feature
 df['SepalLengthCm'].hist()
@@ -87,6 +120,8 @@ plt.xlabel("Sepal Width")
 plt.ylabel("Petal Width")
 plt.legend()
 
+#Correlation Matrix
+
 # Compute the correlation matrix 
 df.corr()
 
@@ -94,6 +129,8 @@ df.corr()
 corr = df.corr()
 fig, ax = plt.subplots(figsize=(5, 4))
 sns.heatmap(corr, annot=True, ax=ax, cmap='coolwarm')
+
+#Model Training
 
 # Split the dataset into training and testing sets
 from sklearn.model_selection import train_test_split
@@ -118,6 +155,8 @@ from sklearn.tree import DecisionTreeClassifier
 model3 = DecisionTreeClassifier()
 model3.fit(x_train, y_train)
 print("Accuracy (Decision Tree): ", model3.score(x_test, y_test) * 100)
+
+#Confusion Matrix
 
 from sklearn.metrics import confusion_matrix
 
